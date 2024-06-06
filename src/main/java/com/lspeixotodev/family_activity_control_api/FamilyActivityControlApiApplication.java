3ed2f3f7580e78277827c026bf889e6accf8a440
lspeixotodev <lspeixotodev@gmail.com>
2024-06-06 11:23:09 -0300

    Start repository

---
+++ src/main/java/com/lspeixotodev/family_activity_control_api/FamilyActivityControlApiApplication.java
@@ -0,0 +1,13 @@
+package com.lspeixotodev.family_activity_control_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyActivityControlApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilyActivityControlApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lspeixotodev/family_activity_control_api/FamilyActivityControlApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lspeixotodev.family_activity_control_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyActivityControlApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
