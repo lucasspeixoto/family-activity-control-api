37c5c1f0f93dfb94f0b094aff22f329746da4271
lspeixotodev <lspeixotodev@gmail.com>
2024-06-16 12:45:02 -0300

    Fix create bill query with right types

--- src/main/resources/db/migration/V1__Create_Table_Bill.sql
+++ src/main/resources/db/migration/V1__Create_Table_Bill.sql
@@ -4,11 +4,11 @@ CREATE TABLE bill(
     id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
     title VARCHAR(50) NOT NULL,
     owner VARCHAR(50) NOT NULL,
-    description VARCHAR(250) NOT NULL,
-    category VARCHAR(50) NOT NULL,
-    type VARCHAR(50) NULL,
     amount NUMERIC NOT NULL,
+    category VARCHAR(50) NOT NULL,
+    description VARCHAR(250) NOT NULL,
     finish_at TIMESTAMP NOT NULL,
+    type VARCHAR(50) NULL,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL
 );
