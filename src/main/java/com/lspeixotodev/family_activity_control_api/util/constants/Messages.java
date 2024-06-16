c231a3943658a8118103ad09b7463f7db3824ca2
lspeixotodev <lspeixotodev@gmail.com>
2024-06-16 12:44:17 -0300

    Change messages to English

--- src/main/java/com/lspeixotodev/family_activity_control_api/util/constants/Messages.java
+++ src/main/java/com/lspeixotodev/family_activity_control_api/util/constants/Messages.java
@@ -1,6 +1,6 @@
 package com.lspeixotodev.family_activity_control_api.util.constants;
 
 public class Messages {
-    public static final String NOT_FOUND_MESSAGE = "não encontrado com";
+    public static final String NOT_FOUND_MESSAGE = "not found with";
 
 }
