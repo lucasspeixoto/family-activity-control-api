1f556090f82a3d8186f9e8c5e992047b177c50d7
lspeixotodev <lspeixotodev@gmail.com>
2024-07-22 20:11:31 -0300

    feat: Create flyway scripts for integration tests

---
+++ src/test/resources/db.migration/V4__Insert_Data_In_Users.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (id, email, name, password, username) VALUES
+	('fd361c69-4657-44da-917f-0ee7c00e491d', 'lspeixotodev@gmail.com', 'Lucas Peixoto', '$2a$10$bd/mCJerWHxIHxRSdCXk3.YVenBj4XI2X99oKfoC7wnNSXU8sIb.O', 'lucas'),
+	('b4c8d28e-a537-4a89-86de-171da4c56c5f', 'admin@admin.com', 'Admin', '$2a$10$5vRT6BF51vrpV8aeDO39fOfhphUc3li5KEEkosKWWcn2Ug27HnjpG', 'admin');
