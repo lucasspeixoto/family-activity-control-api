df45228101bfb8f56159f1d80523bc32ce34789c
lspeixotodev <lspeixotodev@gmail.com>
2024-07-22 20:13:10 -0300

    feat: Change Security config with unnecessary method calls and change base auth api endpoint

--- src/main/java/com/lspeixotodev/family_activity_control_api/config/SecurityConfig.java
+++ src/main/java/com/lspeixotodev/family_activity_control_api/config/SecurityConfig.java
@@ -21,9 +21,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+//@Profile("!test")
 @Configuration
 @EnableMethodSecurity
-@Profile("!test")
 public class SecurityConfig {
 
     @Autowired
@@ -58,7 +58,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(
                         (authorize) ->
                                 authorize
-                                        .requestMatchers(HttpMethod.POST, "/auth/**").permitAll()
+                                        .requestMatchers(HttpMethod.POST, "/api/v1/auth/**").permitAll()
                                         .requestMatchers(
                                                 "/swagger-ui/**",
                                                 "/v3/api-docs/**"
@@ -70,9 +70,6 @@ public class SecurityConfig {
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 ).addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
-        http.addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class);
-
         return http.build();
     }
-
 }
